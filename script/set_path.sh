0b001534b771b47b35a59930d18d913b44bb9e02
tykim0507 <nowtyk@snu.ac.kr>
2025-12-14 18:01:11 +0900

    Modify path setting to support zsh

--- script/set_path.sh
+++ script/set_path.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 ##################### Paths #####################
 #### ReinFlow Authors revised from DPPO's original repository
@@ -9,13 +9,13 @@ DEFAULT_DATA_DIR="${PWD}/data"
 DEFAULT_LOG_DIR="${PWD}/log"
 
 # Prompt the user for input, allowing overrides
-read -p "Enter the place where your reinflow script lies: [default: ${DEFAULT_DIR}], press ENTER to use default: " DIR
+read "?Enter the place where your reinflow script lies: [default: ${DEFAULT_DIR}], press ENTER to use default: " DIR
 REINFLOW_DIR=${DIR:-$DEFAULT_DIR}  # Use user input or default if input is empty
 
-read -p "Enter the desired data directory [default: ${DEFAULT_DATA_DIR}], press ENTER to use default: " DATA_DIR
+read "?Enter the desired data directory [default: ${DEFAULT_DATA_DIR}], press ENTER to use default: " DATA_DIR
 REINFLOW_DATA_DIR=${DATA_DIR:-$DEFAULT_DATA_DIR}  # Use user input or default if input is empty
 
-read -p "Enter the desired logging directory [default: ${DEFAULT_LOG_DIR}], press ENTER to use default: " LOG_DIR
+read "?Enter the desired logging directory [default: ${DEFAULT_LOG_DIR}], press ENTER to use default: " LOG_DIR
 REINFLOW_LOG_DIR=${LOG_DIR:-$DEFAULT_LOG_DIR}  # Use user input or default if input is empty
 
 # Export to current session
@@ -28,25 +28,25 @@ echo "Script directory set to: $REINFLOW_DIR"
 echo "Data directory set to: $REINFLOW_DATA_DIR"
 echo "Log directory set to: $REINFLOW_LOG_DIR"
 
-# Append environment variables to .bashrc
-echo "export REINFLOW_DIR=\"$REINFLOW_DIR\"" >> ~/.bashrc
-echo "export REINFLOW_DATA_DIR=\"$REINFLOW_DATA_DIR\"" >> ~/.bashrc
-echo "export REINFLOW_LOG_DIR=\"$REINFLOW_LOG_DIR\"" >> ~/.bashrc
+# Append environment variables to .zshrc
+echo "export REINFLOW_DIR=\"$REINFLOW_DIR\"" >> ~/.zshrc
+echo "export REINFLOW_DATA_DIR=\"$REINFLOW_DATA_DIR\"" >> ~/.zshrc
+echo "export REINFLOW_LOG_DIR=\"$REINFLOW_LOG_DIR\"" >> ~/.zshrc
 
-echo "Environment variables REINFLOW_DIR, REINFLOW_DATA_DIR and REINFLOW_LOG_DIR added to .bashrc and applied to the current session."
+echo "Environment variables REINFLOW_DIR, REINFLOW_DATA_DIR and REINFLOW_LOG_DIR added to .zshrc and applied to the current session."
 
 # Set verbose logging
 echo -e "# verbose debug
 export D4RL_SUPPRESS_IMPORT_ERROR=1
 export HYDRA_FULL_ERROR=1
 export CUDA_LAUNCH_BLOCKING=1
-export TORCH_USE_CUDA_DSA=1">> ~/.bashrc
+export TORCH_USE_CUDA_DSA=1">> ~/.zshrc
 echo "Suppressed D4RL import errors, turned on verbose debugging for HYDRA, CUDA, and TORCH_USE_CUDA_DSA"
 
 ##################### WandB #####################
 
 # Prompt the user for input, allowing overrides
-read -p "Enter your WandB entity (username or team name), press ENTER to skip: " ENTITY
+read "?Enter your WandB entity (username or team name), press ENTER to skip: " ENTITY
 
 # Check if ENTITY is not empty
 if [ -n "$ENTITY" ]; then
@@ -56,10 +56,10 @@ if [ -n "$ENTITY" ]; then
   # Confirm the entity with the user
   echo "WandB entity set to: $REINFLOW_WANDB_ENTITY"
 
-  # Append environment variable to .bashrc
-  echo "export REINFLOW_WANDB_ENTITY=\"$ENTITY\"" >> ~/.bashrc
+  # Append environment variable to .zshrc
+  echo "export REINFLOW_WANDB_ENTITY=\"$ENTITY\"" >> ~/.zshrc
   
-  echo "Environment variable REINFLOW_WANDB_ENTITY added to .bashrc and applied to the current session."
+  echo "Environment variable REINFLOW_WANDB_ENTITY added to .zshrc and applied to the current session."
 else
   # If ENTITY is empty, skip setting the environment variable
   echo "No WandB entity provided. Please set wandb=null when running scripts to disable wandb logging and avoid error."
